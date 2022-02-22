8dee7b525ad1a850b1968ed5d2ffd7c1eded9893
Bony Limas <bonyl@gmail.com>
2022-02-22 16:50:50 +1100

    Update Logic

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -8,14 +8,13 @@ namespace FizzBuzz
         {
             for (int i = 1; i <= 100; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                    Console.WriteLine("FizzBuzz");
-                else if (i % 5 == 0)
-                    Console.WriteLine("Buzz");
-                else if (i % 3 == 0)
-                    Console.WriteLine("Fizz");
-                else
-                    Console.WriteLine(i);
+                string toDisplay = "";
+
+                toDisplay = i % 3 == 0 ? toDisplay + "Fizz" : toDisplay;
+                toDisplay = i % 5 == 0 ? toDisplay + "Buzz" : toDisplay;
+                toDisplay = toDisplay.Length <= 0 ? i.ToString() : toDisplay;
+
+                Console.WriteLine(toDisplay);
             }
         }
     }
